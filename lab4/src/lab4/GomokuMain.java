e0e81f495a93f2aa3938cc4015af833e038c0b0e
robinvp <robinwp@hotmail.se>
2017-02-15 16:12:03 +0100

    hejsan

---
+++ lab4/src/lab4/GomokuMain.java
@@ -0,0 +1,17 @@
+import lab4.data.*;
+import lab4.client.*;
+import lab4.gui.*;
+
+package lab4;
+
+public class GomokuMain {
+private int port = 4001;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("doaks");
+		System.out.println("doksa");
+		System.out.println("dask");
+	}
+
+}
