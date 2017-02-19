48c0ef6846767d930b5f4c15479fd6b5a88ed3c8
Tormentar <Tormentar@live.se>
2017-02-19 18:35:43 +0100

    Finished GamePanel, paintComponent draws X for OTHER and circles for ME

--- lab4/src/lab4/gui/GamePanel.java
+++ lab4/src/lab4/gui/GamePanel.java
@@ -1,13 +1,10 @@
 package lab4.gui;
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.util.Observable;
 import java.util.Observer;
-
 import javax.swing.JPanel;
-
 import lab4.data.GameGrid;
 
 /**
@@ -15,7 +12,6 @@ import lab4.data.GameGrid;
  */
 
 public class GamePanel extends JPanel implements Observer{
-
 	private final int UNIT_SIZE = 20;
 	private GameGrid grid;
 	
@@ -44,7 +40,8 @@ public class GamePanel extends JPanel implements Observer{
 	 * @return an integer array containing the [x, y] grid position
 	 */
 	public int[] getGridPosition(int x, int y){
-		
+		int[] array = {x / UNIT_SIZE, y / UNIT_SIZE};
+		return array;
 	}
 	
 	public void update(Observable arg0, Object arg1) {
@@ -53,7 +50,21 @@ public class GamePanel extends JPanel implements Observer{
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		
+		g.setColor(Color.BLACK);
+		for (int x = 0; x < grid.getSize(); x++) {
+			for(int y = 0; y < grid.getSize(); y++) {
+				g.fillRect(y*UNIT_SIZE, x*UNIT_SIZE, UNIT_SIZE*(y + 1), UNIT_SIZE*(x + 1));
+				if (grid.getLocation(y, x) == grid.ME) {
+					g.fillOval(y*UNIT_SIZE, x*UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
+				} if(grid.getLocation(y, x) == grid.OTHER) {
+					// if this does not work, just use fillOval with a different color (same as above)
+					//like this:
+					// g.setColor(Color.BLACK);
+					// g.fillOval(y*UNIT_SIZE, x*UNIT_SIZE, UNIT_SIZE, UNIT_SIZE);
+					g.drawLine(x, y, x+UNIT_SIZE, y+ UNIT_SIZE);
+					g.drawLine(x+UNIT_SIZE, y, x, y+ UNIT_SIZE);
+					}
+				}
+			}
+		}
 	}
-	
-}
